454a40ea758d213faf87e9a794f66aefc72a7112
Suvanshsehgal <suvanshsehgal@gmail.com>
2026-01-10 22:51:59 +0530

    Created a fadein animation for smooth transition

--- lawlens-frontend/tailwind.config.js
+++ lawlens-frontend/tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      animation:{
+        fadeIn: "fadeIn 0.4s ease-in-out",
+      },
       fontFamily: {
         'playfair': ["'Playfair Display'", "serif"],
         'inter': ["'Inter'", "sans-serif"],
